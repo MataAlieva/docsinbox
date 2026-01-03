9ade0204676c35a72d0469e211ac61f25cb9b929
MataAlieva <mataalieva58@gmail.com>
2026-01-03 22:43:34 +0300

    added

---
+++ src/test/java/pages/DocsInBoxPage.java
@@ -0,0 +1,71 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DocsInBoxPage {
+
+    // Локаторы (Элементы)
+    private final SelenideElement
+            tariffsMenu = $(byText("Тарифы")),
+            loginMenuButton = $(byText("Войти")),
+            demoMenuButton = $(byText("Записаться на демо")),
+            halfYearDiscountButton = $(".tn-atom__button-content"),
+
+    // Форма входа
+    loginInput = $("#loginForm_login"),
+            passwordInput = $("#loginForm_password"),
+            submitLoginButton = $(byText("Войти")),
+
+    // Форма Демо (используем name, так как ID могут меняться)
+    demoNameInput = $("[name='name']"),
+            demoPhoneInput = $("[name='phone']"),
+            demoEmailInput = $("[name='email']"),
+            submitDemoButton = $(".t-btn_effects");
+
+    // Действия (Методы)
+    @Step("Открыть главную страницу")
+    public DocsInBoxPage openPage() {
+        open("https://docsinbox.ru/");
+        return this;
+    }
+
+    @Step("Перейти в раздел Тарифы")
+    public void goToTariffs() {
+        tariffsMenu.click();
+    }
+
+    @Step("Выбрать тариф на полгода со скидкой")
+    public void clickHalfYearDiscount() {
+        halfYearDiscountButton.click();
+    }
+
+    @Step("Открыть форму входа")
+    public void openLoginForm() {
+        loginMenuButton.click();
+    }
+
+    @Step("Заполнить форму входа: логин {login}, пароль {password}")
+    public void fillLoginForm(String login, String password) {
+        loginInput.shouldBe(visible).setValue(login);
+        passwordInput.shouldBe(visible).setValue(password);
+        submitLoginButton.click();
+    }
+
+    @Step("Открыть форму записи на демо")
+    public void openDemoForm() {
+        demoMenuButton.click();
+    }
+
+    @Step("Заполнить форму демо: имя {name}, телефон {phone}, почта {email}")
+    public void fillDemoForm(String name, String phone, String email) {
+        demoNameInput.shouldBe(visible).setValue(name);
+        demoPhoneInput.setValue(phone);
+        demoEmailInput.setValue(email);
+        submitDemoButton.click();
+    }
+}
---
+++ src/test/java/tests/DocsInBoxTests.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.DocsInBoxPage;
+
+public class DocsInBoxTests extends TestBase {
+
+    DocsInBoxPage docsPage = new DocsInBoxPage();
+
+    @Test
+    @DisplayName("Переход в Тарифы")
+    void testTariffsNavigation() {
+        docsPage.openPage()
+                .goToTariffs();
+    }
+
+    @Test
+    @DisplayName("Переход в Тарифы, Полгода -10%")
+    void testHalfYearTariff() {
+        docsPage.openPage();
+        docsPage.goToTariffs();
+        docsPage.clickHalfYearDiscount();
+    }
+
+    @Test
+    @DisplayName("Некорректное заполнение формы Войти")
+    void testInvalidLogin() {
+        docsPage.openPage();
+        docsPage.openLoginForm();
+        docsPage.fillLoginForm("Matama", "12ws");
+    }
+
+    @Test
+    @DisplayName("Пустая форма Войти")
+    void testEmptyLoginForm() {
+        docsPage.openPage();
+        docsPage.openLoginForm();
+    }
+
+    @Test
+    @DisplayName("Некорректное заполнение формы Демо")
+    void testInvalidDemoSignUp() {
+        docsPage.openPage();
+        docsPage.openDemoForm();
+        docsPage.fillDemoForm("Matama", "9269999999", "Mata@gmail.com");
+    }
+}
